31a0da85d3512fa0aab92cce25b7e3f5b38510cd
andrey.verenich <andrey.verenich@moneyman.ru>
2025-11-08 13:18:53 +0300

    push to postgres

---
+++ ecommerce-batch-pipeline/postgres/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS log_events (
+                                          id SERIAL PRIMARY KEY,
+                                          timestamp TIMESTAMP,
+                                          level TEXT,
+                                          event TEXT,
+                                          userId TEXT,
+                                          sessionId TEXT,
+                                          device TEXT,
+                                          page TEXT,
+                                          productId TEXT,
+                                          category TEXT,
+                                          errorType TEXT,
+                                          paymentMethod TEXT,
+                                          minute TEXT
+);
--- ecommerce-batch-pipeline/spark-app/src/main/java/com/example/SimpleSparkJob.java
+++ ecommerce-batch-pipeline/spark-app/src/main/java/com/example/SimpleSparkJob.java
@@ -1,7 +1,13 @@
 package com.example;
 
-import org.apache.spark.api.java.function.FilterFunction;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.collect_list;
+import static org.apache.spark.sql.functions.date_format;
+import static org.apache.spark.sql.functions.desc;
+import static org.apache.spark.sql.functions.to_timestamp;
+
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class SimpleSparkJob {
@@ -9,18 +15,36 @@ public class SimpleSparkJob {
   public static void main(String[] args) throws InterruptedException {
     Thread.sleep(10_000);
     SparkSession spark = SparkSession.builder()
-        .appName("Log Analyzer")
+        .appName("Ecommerce Log Analyzer")
         .master("spark://spark-master:7077")
         .config("spark.hadoop.fs.defaultFS", "hdfs://namenode:8020")
         .getOrCreate();
 
-    Dataset<String> logs = spark.read().textFile("hdfs://namenode:8020/logs/logs.txt");
+    Dataset<Row> logs = spark.read().json("hdfs://namenode:8020/logs/logs.txt");
 
-    long totalLines = logs.count();
-    long errorLines = logs.filter((FilterFunction<String>) line -> line.contains("ERROR")).count();
+    logs = logs.withColumn("minute", date_format(col("timestamp"), "yyyy-MM-dd HH:mm"));
+    logs = logs.withColumn(
+        "timestamp",
+        to_timestamp(col("timestamp"), "yyyy-MM-dd'T'HH:mm:ss")
+    );
 
-    System.out.println("📄 Total log lines: " + totalLines);
-    System.out.println("❌ Error log lines: " + errorLines);
+    logs.groupBy("event").count().orderBy(desc("count")).show();
+    logs.filter(col("level").equalTo("ERROR")).groupBy("errorType").count().show();
+    logs.groupBy("minute").count().orderBy("minute").show();
+    logs.groupBy("userId").agg(collect_list("event").alias("events")).show(false);
+    logs.filter(col("event").equalTo("Product viewed")).groupBy("category").count().show();
+    logs.groupBy("device").count().show();
+
+    logs.write()
+        .mode("append")
+        .format("jdbc")
+        .option("url", "jdbc:postgresql://postgres:5432/ecommerce_logs")
+        .option("dbtable", "log_events")
+        .option("user", "spark")
+        .option("password", "sparkpass")
+        .option("driver", "org.postgresql.Driver")
+        .save();
+    System.out.println("FINISHED");
 
     spark.stop();
   }
