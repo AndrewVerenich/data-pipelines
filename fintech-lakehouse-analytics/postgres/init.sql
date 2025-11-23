ace5c80672296f57a79a4565ce778553df67e769
AndrewVerenich <26678010+AndrewVerenich@users.noreply.github.com>
2025-11-23 15:39:26 +0300

    dbt

--- fintech-lakehouse-analytics/postgres/init.sql
+++ fintech-lakehouse-analytics/postgres/init.sql
@@ -13,12 +13,43 @@ CREATE TABLE transactions (
     created_at TIMESTAMP DEFAULT now()
 );
 
-INSERT INTO users (name, country) VALUES
+INSERT INTO users (name, country)
+VALUES
     ('Alice', 'USA'),
     ('Bob', 'Germany'),
-    ('Charlie', 'France');
+    ('Charlie', 'France'),
+    ('Diana', 'UK'),
+    ('Eve', 'Canada'),
+    ('Frank', 'Japan'),
+    ('Grace', 'Italy'),
+    ('Heidi', 'Spain'),
+    ('Ivan', 'Russia'),
+    ('Judy', 'Brazil'),
+    ('Karl', 'Sweden'),
+    ('Laura', 'Norway'),
+    ('Mallory', 'Poland'),
+    ('Niaj', 'India'),
+    ('Olivia', 'China'),
+    ('Peggy', 'Australia'),
+    ('Quentin', 'Mexico'),
+    ('Rupert', 'Netherlands'),
+    ('Sybil', 'Switzerland'),
+    ('Trent', 'Austria');
 
-INSERT INTO transactions (user_id, amount, status) VALUES
-    (1, 100.50, 'SUCCESS'),
-    (2, 200.00, 'FAILED'),
-    (3, 300.75, 'SUCCESS');
+DO $$
+DECLARE
+i INT;
+    uid INT;
+    amt NUMERIC;
+    st TEXT;
+    dt TIMESTAMP;
+BEGIN
+FOR i IN 1..10000 LOOP
+        uid := (random()*19 + 1)::INT; -- случайный user_id от 1 до 20
+        amt := round((random()*3000 + 5)::NUMERIC, 2); -- сумма от 5 до 3005
+        st := (ARRAY['SUCCESS','FAILED','PENDING'])[ceil(random()*3)];
+        dt := now() - (random()*30 || ' days')::interval; -- случайная дата за последние 30 дней
+INSERT INTO transactions (user_id, amount, status, created_at)
+VALUES (uid, amt, st, dt);
+END LOOP;
+END $$;
